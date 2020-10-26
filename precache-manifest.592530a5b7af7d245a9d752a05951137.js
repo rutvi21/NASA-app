6756af98ed47ba676dcc35706bfa3cfd13d677e9
Rutvi Tilala <37977465+rutvi21@users.noreply.github.com>
2020-10-27 02:18:50 +0400

    Updates

---
+++ precache-manifest.592530a5b7af7d245a9d752a05951137.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1152067ffee769cf842f492b472cad07",
+    "url": "/NASA-app/index.html"
+  },
+  {
+    "revision": "050bb98c167ebe575240",
+    "url": "/NASA-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "37fbc193ac99ebe4e45f",
+    "url": "/NASA-app/static/js/2.bc898789.chunk.js"
+  },
+  {
+    "revision": "2d54400cfeffd5a03c97473febb60b1e",
+    "url": "/NASA-app/static/js/2.bc898789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "050bb98c167ebe575240",
+    "url": "/NASA-app/static/js/main.a1a08580.chunk.js"
+  },
+  {
+    "revision": "10da48a40c29f7810ef3",
+    "url": "/NASA-app/static/js/runtime-main.4fb30023.js"
+  },
+  {
+    "revision": "088fe5a9e8ff9098c09de7e952eed2ea",
+    "url": "/NASA-app/static/media/asteroid.088fe5a9.png"
+  }
+]);
